0c5c691ab3dd863c3ba8ae853f190fdf0debd488
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-02-16 23:27:31 -0500

    removed debug db

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires=[
 ]
 
 if __name__ == "__main__":
-    taskbook_db = dataset.connect('sqlite:///taskbook2.db')
+    taskbook_db = dataset.connect('sqlite:///taskbook.db')
     task_table = taskbook_db.get_table('task')
     task_table.drop()
     task_table = taskbook_db.create_table('task')
