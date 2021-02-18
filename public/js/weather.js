9a17a70fcb0a30e69bbff9f439bfecb71dc5799b
Nolan Spencer <nspencer3513@gmail.com>
2021-02-18 15:04:30 -0500

    Compiler error for span tags

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
-var usercity;
-var country;
-var state;
-
-$.getJSON("http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1", function(moreData){
+/*$.getJSON("http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1", function(moreData){
     console.log(moreData);
-    usercity = moreData.city;
-    country = moreData.country_code;
-    state = moreData.region_code;
-=======
-$.getJSON("http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1", function(moreData){
-    console.log(moreData);
-    var usercity = moreData.city;
+    var city = moreData.city;
     var country = moreData.country_code;
     var state = moreData.region_code;
->>>>>>> feee7bfb923d9db1e9de2704615b08c9195ead8d
-    $(".usercity").append(usercity);
+    $(".usercity").append(city);
     $(".country").append(country);
     $(".state").append(state);
 });
-<<<<<<< HEAD
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + usercity + "," + state + "," + country + "&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5", function(data) {
-=======
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + usercity + "," + state + "," country + "&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5", function(data) {
->>>>>>> feee7bfb923d9db1e9de2704615b08c9195ead8d
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=cuyahoga falls,oh,us&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5", function(data) {
  console.log(data);
 
  var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
@@ -37,4 +21,18 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + usercity + "," +
  $(".weather").append(weather);
  $(".city").append(city);
 
-});
+});*/
+(async () => {
+    let { city: usercity, country_code: ocountry, region_code: state } = await (await fetch('http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1')).json();
+    let weather_data = await (await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${usercity}, ${state}, ${ocountry}&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5`)).json();
+    let icon = `https://openweathermap.org/img/w/${weather_data.weather[0].icon}.png`;
+    let { main: { temp }, name: city, sys: {country} } = weather_data;
+    let weather = weather_data.weather[0].main;
+  
+    $(".state").append(state);
+    $(".icon").attr("src", icon);
+    $(".temp").append(Math.floor(temp));
+    $(".weather").append(weather);
+    $(".city").append(city);
+    $(".country").append(country);
+  })();
