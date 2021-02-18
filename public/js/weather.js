feee7bfb923d9db1e9de2704615b08c9195ead8d
Nolan Spencer <nspencer3513@gmail.com>
2021-02-18 11:49:50 -0500

    String broken in js code

---
+++ public/js/weather.js
@@ -0,0 +1,23 @@
+$.getJSON("http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1", function(moreData){
+    console.log(moreData);
+    var usercity = moreData.city;
+    var country = moreData.country_code;
+    var state = moreData.region_code;
+    $(".usercity").append(usercity);
+    $(".country").append(country);
+    $(".state").append(state);
+});
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + usercity + "," + state + "," country + "&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5", function(data) {
+ console.log(data);
+
+ var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+ var temp = data.main.temp;
+ var weather = data.weather[0].main;
+ var city = data.name;
+
+ $(".icon").attr("src", icon);
+ $(".temp").append(Math.floor(temp));
+ $(".weather").append(weather);
+ $(".city").append(city);
+
+});
