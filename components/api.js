b407b0363d711f01e816a63be15708c98e9c852e
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-02-18 13:53:30 -0500

    Implemented LocalStorage API

--- components/api.js
+++ components/api.js
@@ -1,20 +1,49 @@
 
 
+let template_api_data = {
+  tasks: [],
+}
+
 let local = {
+  get $data() {
+    let data = localStorage.getItem('api_data');
+    if (!data) {
+      data = JSON.stringify(template_api_data);
+    }
+    return JSON.parse(data);
+  },
+  set $data(value) {
+    localStorage.setItem('api_data', JSON.stringify(value))
+  },
   tasks: {
-    async create(data) {
-      console.log('CREATING LOCAL TASK', data);
+    async create(taskdata) {
+      console.log('CREATING LOCAL TASK', taskdata);
+      let data = local.$data;
+      let maxid = Math.max(0, ...data.tasks.filter(o => o && o.id).map(o => Number(o.id)));
+      taskdata.id = maxid + 1;
+      data.tasks.push(taskdata);
+      local.$data = data;
     },
     async update(task) {
       console.log('UPDATING LOCAL TASK', task);
+      let data = local.$data;
+      Object.assign(data.tasks.find(o => o.id == task.id), task.data);
+      local.$data = data;
     },
     async delete(task) {
       console.log('DELETING LOCAL TASK', task);
+      let data = local.$data;
+      console.log(data.tasks.findIndex(o => o.id == task.data.id))
+      delete data.tasks[data.tasks.findIndex(o => o.id == task.data.id)];
+      data.tasks = data.tasks.filter(o => o)
+      local.$data = data;
     },
     async get() {
       console.log('FETCHING LOCAL TASKS');
+      let { tasks } = local.$data;
+      console.log(tasks);
       return {
-        tasks: [],
+        tasks,
       }
     },
   },
