00f3efa51074b11ad3123f9d0cd1c6658ea6f607
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-02-21 15:50:50 -0500

    added https to weather

--- public/js/weather.js
+++ public/js/weather.js
@@ -23,8 +23,8 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=cuyahoga falls,oh,us
 
 });*/
 (async () => {
-  let { city: usercity, country_code: ocountry, region_code: state } = await (await fetch('http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1')).json();
-  let weather_data = await (await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${usercity}, ${state}, ${ocountry}&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5`)).json();
+  let { city: usercity, country_code: ocountry, region_code: state } = await (await fetch('https://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1')).json();
+  let weather_data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${usercity}, ${state}, ${ocountry}&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5`)).json();
   let icon = `https://openweathermap.org/img/w/${weather_data.weather[0].icon}.png`;
   let { main: { temp }, name: city, sys: { country } } = weather_data;
   let weather = weather_data.weather[0].main;
