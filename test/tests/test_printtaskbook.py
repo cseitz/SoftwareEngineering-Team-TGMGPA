5e28b74eb793643e7c7952824b4650d64d2b32df
joel lee <jrnixdata@gmail.com>
2021-02-26 16:48:17 -0500

    updated first task to find taskbook and other today tasks

--- test/tests/test_printtaskbook.py
+++ test/tests/test_printtaskbook.py
@@ -7,8 +7,13 @@ driver.get("http://localhost:8080")
 
 
 #grab taskbook text
-
+elem = driver.find_element_by_xpath("/html/body/div[1]/span[1]/b")
 #print taskbook text in cmd line
-
+print("Taskbook found")
 #print all tasks in today column
-print("hello")
+#Today0 = driver.find_element_by_xpath('//*[@id="task-2"]/div[1]/div/span')
+print("first task found")
+#Today1 = driver.find_element_by_xpath('//*[@id="task-1"]/div[1]/div/span')
+print("second task found")
+
+test = input("press enter when done")
