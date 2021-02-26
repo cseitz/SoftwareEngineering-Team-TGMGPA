c60aa75f5b34c264fb7087791da070fa100ad69c
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-02-26 12:05:42 -0500

    working task counter backend

--- components/archives.js
+++ components/archives.js
@@ -1,10 +1,29 @@
 
 let previousDeletion;
 
+const DAY = 24 * 60 * 60 * 1000;
+const MONTH = 30 * DAY;
 var Archives = {
+  get archive() {
+    let data = localStorage.getItem('archive_data');
+    if (data) {
+      let OneMonthOld = Date.now() - MONTH;
+      return JSON.parse(data).filter(o => o.deleted_at > OneMonthOld);
+    } else {
+      return [];
+    }
+  },
+  set archive(task) {
+    let OneMonthOld = Date.now() - MONTH;
+    localStorage.setItem('archive_data', JSON.stringify([...this.archive, task].filter(o => o.deleted_at > OneMonthOld)));
+  },
   create(task) {
     previousDeletion = task.data;
     console.log('TASK WAS DELETED', task.data);
+    this.archive = {
+      ...task.data,
+      deleted_at: Date.now(),
+    }
   },
   undoDelete() {
     if (previousDeletion) {
@@ -12,5 +31,9 @@ var Archives = {
       previousDeletion = false;
       TaskList.create(task);
     }
-  }
+  },
+  completed(days = 7) {
+    let TimeRange = Date.now() - (DAY * days);
+    return this.archive.filter(o => o.deleted_at > TimeRange).filter(o => o.completed).length;
+  },
 }
