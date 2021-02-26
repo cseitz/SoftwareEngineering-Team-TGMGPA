154e52c429edb0dcc2494e5b623000454c84f466
joel lee <jrnixdata@gmail.com>
2021-02-26 15:10:46 -0500

    updating first task test

--- test/tests.py
+++ test/tests.py
@@ -11,5 +11,5 @@ elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 
 import test_example
-
+import test_printtaskbook
 driver.close()
---
+++ test/tests/test_printtaskbook.py
@@ -0,0 +1,14 @@
+
+
+from driver import driver, Keys
+
+driver.get("http://localhost:8080")
+#driver.get("http://google.com")
+
+
+#grab taskbook text
+
+#print taskbook text in cmd line
+
+#print all tasks in today column
+print("hello")
