daadf71805b6beeb2b978803a31457c1b6fd02c1
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-02-26 14:04:59 -0500

    okay its good now

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-#exec(open("./test/test.py").read())
-#import importlib
-#importlib.import_module('./test/test.py', package='')
 import sys
 sys.path.append('./test')
+import driver
 import tests
--- test/tests.py
+++ test/tests.py
@@ -1,20 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.keys import Keys
-global driver
-global browser
-import os
-#import subprocess
-from platform import uname
-if os.name == 'posix' and 'Microsoft' in uname().release: # running WSL
-    print(os.system('cmd.exe /c "python test.py"'))
-    quit()
-elif os.name == 'posix':
-    driver = webdriver.Firefox()
-else:
-    driver = webdriver.Firefox(executable_path=r'test/drivers/geckodriver.exe')
 
-browser = driver
+from driver import driver, Keys
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
