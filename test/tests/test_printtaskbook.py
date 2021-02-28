d5182dd6e33d4a28676dce499bf9eaf5a0d5be7d
joel lee <jrnixdata@gmail.com>
2021-02-28 17:30:52 -0500

    Finished testing website automation to grab tasks and print

--- test/tests/test_printtaskbook.py
+++ test/tests/test_printtaskbook.py
@@ -8,12 +8,20 @@ driver.get("http://localhost:8080")
 
 #grab taskbook text
 elem = driver.find_element_by_xpath("/html/body/div[1]/span[1]/b")
-#print taskbook text in cmd line
-print("Taskbook found")
-#print all tasks in today column
-#Today0 = driver.find_element_by_xpath('//*[@id="task-2"]/div[1]/div/span')
-print("first task found")
-#Today1 = driver.find_element_by_xpath('//*[@id="task-1"]/div[1]/div/span')
-print("second task found")
 
+#converts elem into a string
+elemtxt = elem.text
+
+#print taskbook text in cmd line
+print("Taskbook found: " + elemtxt)
+
+#print all tasks in today column
+Today0 = driver.find_element_by_xpath('//*[@id="task-2"]/div[1]/div/span')
+txt0 = Today0.text
+print("first task found: " + txt0)
+Today1 = driver.find_element_by_xpath('//*[@id="task-1"]/div[1]/div/span')
+txt1 = Today1.text
+print("second task found: " + txt1)
+
+#placeholder so window doesnt close
 test = input("press enter when done")
