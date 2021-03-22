bc403458f6542d97508c91999d4b175a91f1346b
Nolan Spencer <nspencer3513@gmail.com>
2021-03-22 00:26:17 -0400

    Button doesn't work because I don't understand how asynch() works

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,17 +1,16 @@
-$('#location-button').click(function(){
-  if(navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(function(position){
-        console.log(position);
-      });
-  else
-      console.log("geolocation is not supported");
-});
-
-
-
+function Myfunction(){
+  $("#location-button").click(function(){
+    if(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(function(position){
+          console.log(position);
+        });
+    else
+        console.log("geolocation is not supported");
+  });
 (async () => {
-  let { city: usercity, country_code: ocountry, region_code: state } = await (await fetch('http://api.ipstack.com/check?access_key=5448cfde5052f498a30a03b5055fdf3e&format=1')).json();
-  let weather_data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${usercity}, ${state}, ${ocountry}&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5`)).json();
+  var lat = Myfunction.coords.latitude;
+  var lon = Myfunction.coords.longitude;
+  let weather_data = await (await fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=568ed0d76658cf9e941fc1969416ddd5`)).json();
   let icon = `https://openweathermap.org/img/w/${weather_data.weather[0].icon}.png`;
   let { main: { temp }, name: city, sys: { country } } = weather_data;
   let weather = weather_data.weather[0].main;
@@ -23,3 +22,4 @@ $('#location-button').click(function(){
   //$(".city").append(city);
   //$(".country").append(country);
 })();
+}
